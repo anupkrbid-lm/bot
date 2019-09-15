c3ae336130b2be4e8019d9342b2675f330ab1394
Anup Kumar Bid <40849370+anupkrbid-lm@users.noreply.github.com>
2019-09-15 14:14:08 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const SteamUser = require('steam-user');
 const config = require('./config');
 
-const client = new SteamUser();
+const client = new SteamUser({ enablePicsCache: true });
 
 const logOnOptions = {
   accountName: config.username,
@@ -10,9 +10,40 @@ const logOnOptions = {
 
 client.logOn(logOnOptions);
 
-client.on('loggedOn', () => {
+client.on('loggedOn', (details) => {
   console.log('Logged into Steam!');
 
   client.setPersona(SteamUser.Steam.EPersonaState.Online);
-  client.gamesPlayed(440);
+  var currentGame = 0;
+  var games = [
+    385200,
+    596440,
+    680620,
+    368900,
+    259360,
+    410590,
+    304390,
+    460660,
+    431270,
+    253920,
+    254060,
+    351030,
+    35450,
+    233130,
+    279640,
+    892760
+  ];
+  client.gamesPlayed(games[currentGame++]);
+
+
+  setTimeout(function() {
+    client.gamesPlayed(games[currentGame++]);
+
+    if (currentGame === games.length) {
+        currentGame = 0;
+    }
+
+  }, 10800000)
 });
+
+
